30986a505c692587a7d58852798fe5d8f3ddd2ec
Larita <Yatsenko@gmail.com>
2025-10-03 16:22:17 +0300

    feature: 添加折扣系统

--- src/cargo.py
+++ src/cargo.py
@@ -1,18 +1,20 @@
 class Cargo:
-    """货物类 - 管理货物信息"""
-    def __init__(self, weight):
+    """货物类 - 管理货物信息和折扣"""
+    def __init__(self, weight, discount=0):
         self.weight = weight
+        self.discount = discount
 
     def get_base_price(self):
         """根据重量计算基础价格"""
         if self.weight <= 50:
-            return 300
+            base = 300
         elif self.weight <= 100:
-            return 1000
+            base = 1000
         elif self.weight <= 300:
-            return 2000
+            base = 2000
         else:
-            return 2000 + (self.weight - 300) * 10
+            base = 2000 + (self.weight - 300) * 10
+        return base * (1 - self.discount)
 
     def __str__(self):
-        return f"货物重量: {self.weight}kg"
+        return f"货物重量: {self.weight}kg, 折扣: {self.discount*100}%"
