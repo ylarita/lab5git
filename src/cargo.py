0440c5b6355ab1b5cb125cc17d20ec770572f66c
Larita <Yatsenko@gmail.com>
2025-10-03 16:22:15 +0300

    bugfix: 调整基础价格表

--- src/cargo.py
+++ src/cargo.py
@@ -4,15 +4,15 @@ class Cargo:
         self.weight = weight
 
     def get_base_price(self):
-        """根据重量计算基础价格"""
+        """根据重量计算基础价格 - 调整后的价格"""
         if self.weight <= 50:
-            return 300
+            return 350
         elif self.weight <= 100:
-            return 1000
+            return 1100
         elif self.weight <= 300:
-            return 2000
+            return 2200
         else:
-            return 2000 + (self.weight - 300) * 10
+            return 2200 + (self.weight - 300) * 10
 
     def __str__(self):
         return f"货物重量: {self.weight}kg"
