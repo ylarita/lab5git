c088e620653e7e61d171e78e1663d0f4bd091f60
ylarita <laritaacenko@gmail.com>
2025-10-03 16:25:52 +0300

    Update cargo.py

--- src/cargo.py
+++ src/cargo.py
@@ -1,10 +1,10 @@
 class Cargo:
-    """货物类 - 管理货物信息"""
+   
     def __init__(self, weight):
         self.weight = weight
 
     def get_base_price(self):
-        """根据重量计算基础价格"""
+        
         if self.weight <= 50:
             return 300
         elif self.weight <= 100:
@@ -15,4 +15,4 @@ class Cargo:
             return 2000 + (self.weight - 300) * 10
 
     def __str__(self):
-        return f"货物重量: {self.weight}kg"
+        return f": {self.weight}kg"
